da53930f8dd1507c407b1e285b3e6eb2dd4acb3b
jef-design <bermejojff97@gmail.com>
2023-10-09 16:40:55 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Projects from "./pages/projects";
+import Projects from "./pages/Projects";
 import About from "./pages/About";
 import ProjectInfo from "./pages/ProjectInfo";
 import Home from "./pages/Home";
